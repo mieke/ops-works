406695e620374bd691fbc139969540b3f71b8c29
Mieke <mieke.mocke@gmail.com>
2015-01-21 06:40:43 -0800

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,16 +1,4 @@
 # Install some stuff.
-["mysql-client"].each do |pkg|
-  package pkg do
-    action :install
-  end
-end
-
-# Replace php.ini file.
-template '/etc/php5/apache2/php.ini' do
+template '/etc/haproxy/haproxy.cfg' do
   source "php.ini.erb"
-end
-
-# Replace apache2.conf file.
-template '/etc/apache2/apache2.conf' do
-  source "apache2.conf.erb"
 end
