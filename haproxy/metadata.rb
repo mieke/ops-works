d558ea5b04ba502a6263df6c97662f2aa00376cd
Mieke <mieke.mocke@gmail.com>
2015-01-22 21:22:26 -0800

    Update metadata.rb

--- haproxy/metadata.rb
+++ haproxy/metadata.rb
@@ -1,16 +1,16 @@
-name             "haproxycustom"
+name             "haproxy"
 description      'Installs and configures haproxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 maintainer       "AWS OpsWorks"
 license          "Apache 2.0"
 version          "1.0.0"
 
-recipe 'haproxycustom', 'Install and configure a HAProxy instance'
-recipe 'haproxycustom::configure', 'reconfigure and restart HAProxy'
+recipe 'haproxy', 'Install and configure a HAProxy instance'
+recipe 'haproxy::configure', 'reconfigure and restart HAProxy'
 
 attribute 'haproxy/backend',
   :display_name => 'Backend',
   :description => 'List of backend services to load balance',
   :required => true,
   :type => 'array'
-depends 'opsworks_commons'
+#depends 'opsworks_commons'
